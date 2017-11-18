220d9bb5c7cddbe465965421fb6c0b9770449e8c
Aellon Krider <aellonkrider@gmail.com>
2017-11-17 20:30:05 -0500

    Add the rest of the call method in the cli.rb

--- lib/adventure/cli.rb
+++ lib/adventure/cli.rb
@@ -1,5 +1,13 @@
 class Adventure::CLI
 	def call
 		puts "Welcome, let's find your next adventure!"
+		puts "Which state would you like to see adventures for?"
+		input = gets.strip.downcase.gsub(' ', '-')
+		puts "What kind of adventure are you in the mood for? Enter the number."
+		puts "\n"
+		list_activities
+		puts "\n"
+		activity = gets.strip.to_i
+		list_adventures
 	end
 end
