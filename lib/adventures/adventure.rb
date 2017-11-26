0f7ecbe6ed1b483e7af5933aee10740e2b0c09b3
Aellon Krider <aellonkrider@gmail.com>
2017-11-26 16:45:41 -0500

    add initialize method, and method to fill in variables from scraping, add all class variable and attr_accessors for variables

--- lib/adventures/adventure.rb
+++ lib/adventures/adventure.rb
@@ -1,2 +1,19 @@
 class Adventures::Adventure
+
+	attr_accessor :title, :location
+
+	@@all = []
+	
+	def self.new_from_scrape(a)
+		self.new(
+			a.css("div.info h3.title").text,
+			a.css("div.info p.location").text.strip
+		)
+	end
+
+	def initialize(title = nil, location = nil)
+		@title = title
+		@location = location
+		@@all << self
+	end
 end
