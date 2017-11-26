792d1dd80d2902720d249ce246ba70775ff70211
Aellon Krider <aellonkrider@gmail.com>
2017-11-26 17:07:39 -0500

    update adventures.rb to require scraper and adventure classes

--- lib/adventures.rb
+++ lib/adventures.rb
@@ -3,6 +3,8 @@ require 'pry'
 require 'nokogiri'
 
 require_relative "./adventures/version"
+require_relative "./adventures/scraper"
+require_relative "./adventures/adventure"
 require_relative "./adventures/cli"
 
 
