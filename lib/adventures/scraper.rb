e6e02d67d3c0220cf213c6e7f1d3983702fbc128
Aellon Krider <aellonkrider@gmail.com>
2017-11-26 16:50:55 -0500

    update scraper.rb with gather_adventures method

--- lib/adventures/scraper.rb
+++ lib/adventures/scraper.rb
@@ -6,4 +6,11 @@ class Adventures::Scraper
 	def scrape_adventure
 		self.get_page.css("div.adventure-card--image")
 	end
+
+	def gather_adventures
+		scrape_adventure.each do |a|
+		 	Adventures::Adventure.new_from_scrape(a)
+		end
+	end
+
 end
