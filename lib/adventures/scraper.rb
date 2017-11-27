febc763383207d027660a0942f733f5d3e0fe240
Aellon Krider <aellonkrider@gmail.com>
2017-11-26 20:52:32 -0500

    update scraper.rb to include activity and state as arguments in the methods, and update get_page for state interpolation

--- lib/adventures/scraper.rb
+++ lib/adventures/scraper.rb
@@ -1,14 +1,14 @@
 class Adventures::Scraper
-	def get_page
-		doc = Nokogiri::HTML(open("https://www.theoutbound.com/new-york/#{activity}"))	
+	def get_page(activity, state)
+		doc = Nokogiri::HTML(open("https://www.theoutbound.com/#{state}/#{activity}"))	
 	end
 	
-	def scrape_adventure
-		self.get_page.css("div.adventure-card--image")
+	def scrape_adventure(activity, state)
+		self.get_page(activity, state).css("div.adventure-card--image")
 	end
 
-	def gather_adventures
-		scrape_adventure.each do |a|
+	def gather_adventures(activity, state)
+		scrape_adventure(activity, state).each do |a|
 		 	Adventures::Adventure.new_from_scrape(a)
 		end
 	end
