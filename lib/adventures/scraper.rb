cddc46e9e39b6ca34252f65e296d9426b6c30d4e
Aellon Krider <aellonkrider@gmail.com>
2017-11-26 23:05:25 -0500

    update scraper.rb with url creators for activity and title

--- lib/adventures/scraper.rb
+++ lib/adventures/scraper.rb
@@ -1,8 +1,12 @@
 class Adventures::Scraper
 	def get_page(activity, state)
-		doc = Nokogiri::HTML(open("https://www.theoutbound.com/#{state}/#{activity}"))	
+		doc = Nokogiri::HTML(open("https://www.theoutbound.com/#{state}/#{activity_url_creator(activity)}"))	
 	end
 	
+	def activity_url_creator(activity)
+		activity.gsub(' ', '-')
+	end
+
 	def scrape_adventure(activity, state)
 		self.get_page(activity, state).css("div.adventure-card--image")
 	end
@@ -11,6 +15,14 @@ class Adventures::Scraper
 		scrape_adventure(activity, state).each do |a|
 		 	Adventures::Adventure.new_from_scrape(a)
 		end
+	end
+
+	def title_url_creator(title)
+		title.downcase.gsub(' ', '-')
+	end
+
+	def scrape_details
+		doc = Nokogiri::HTML(open("https://www.theoutbound.com/#{state}/#{activity_url_creator(activity)}/title_url_creator(title)"))
 	end
 
 end
