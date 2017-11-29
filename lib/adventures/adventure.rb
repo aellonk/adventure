c10e87a24ddcae766ac32c1a2b9e934a8ebde966
Aellon Krider <aellonkrider@gmail.com>
2017-11-29 17:43:38 -0500

    add url to initialize and add attr_accessors for all of the variables for the adventure details

--- lib/adventures/adventure.rb
+++ lib/adventures/adventure.rb
@@ -1,6 +1,6 @@
 class Adventures::Adventure
 
-	attr_accessor :title, :location
+	attr_accessor :title, :location, :url, :suggested_activities, :skill_level, :season, :trail_type, :rt_distance, :elevation_gain, :description
 
 	@@all = []
 
@@ -8,17 +8,19 @@ class Adventures::Adventure
 		self.new(
 			a.css("div.info h3.title").text,
 			a.css("div.info p.location").text.strip
+			"https://www.theoutbound.com/#{a.css("a").attribute("href").text}"
 		)
 	end
 
-	def initialize(title = nil, location = nil)
+	def initialize(title = nil, location = nil, url = nil)
 		@title = title
 		@location = location
+		@url = url
 		@@all << self
 	end
 
 	def self.all
 		@@all
 	end
-	
+
 end
